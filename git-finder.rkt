4363180aeaea7ff2d5a42fcc36af186fa463d277
Siraaj Khandkar <siraaj@khandkar.net>
2021-12-20 00:20:08 -0500

    Implement 2 output formats: tabular and graph

--- git-finder.rkt
+++ git-finder.rkt
@@ -65,17 +65,21 @@
     (sequence->stream (in-lines stdout)))
   (apply stream-append (map find search-paths)))
 
+(define uniq
+  (compose set->list list->set))
+
 (define/contract (find-git-repos hostname search-paths)
   (-> string? (listof path-string?) (listof Repo?))
   (define (root repos) (first (first repos))) ; All roots are the same in a group
   (define (locals repos) (map second repos))
-  (define (remotes repos) (set->list (list->set (append* (map third repos)))))
+  (define (remotes repos) (uniq (append* (map third repos))))
   (map (λ (repos-with-shared-root-commit)
           (Repo hostname
                 (root repos-with-shared-root-commit)
                 (locals repos-with-shared-root-commit)
                 (remotes repos-with-shared-root-commit)))
        (group-by first (stream-fold (λ (repos dir)
+                                       ; TODO git lookups can be done concurrently
                                        (define root (git-dir->root-digest dir))
                                        (define remotes (git-dir->remotes dir))
                                        (define repo (list root dir remotes))
@@ -85,26 +89,74 @@
                                     '()
                                     (find-git-dirs search-paths)))))
 
+(define/contract (print-table repos)
+  (-> (listof Repo?) void?)
+  (define (output hostname root tag locations)
+    (for-each
+      (λ (location)
+         (displayln (string-join (list hostname root tag location) " ")))
+      locations))
+  (for-each
+    (λ (repo)
+       (match-define (Repo hostname root locals remotes) repo)
+       (output hostname root "local" locals)
+       (output hostname root "remote" remotes)
+       (newline) ; So that same-root locations are visually grouped.
+       )
+    repos))
+
+(define/contract (print-graph repos)
+  (-> (listof Repo?) void?)
+  (displayln "digraph {")
+  (for-each
+    (λ (r)
+       ; TODO Color and shape codes for: root, local and remote.
+       ; locals
+       (match r
+         [(Repo _hostname root (and locals (list* _ _ _)) _)
+          (for-each
+            (λ (l) (printf "~v -> ~v;~n" root l))
+            locals)]
+         [_ (void)])
+
+       ; remotes
+       (match r
+         [(Repo _hostname root _ (and remotes (list* _ _ _)))
+          (for-each
+            (λ (r) (printf "~v -> ~v;~n" root r))
+            remotes)]
+         [_ (void)])
+       )
+    repos)
+  (displayln "}"))
+
 (module+ main
   ; TODO handle sub commands:
   ; - TODO "collect" data for current host
   ; - TODO "integrate" data from per-host data files into a graphviz file
-  (command-line
-    #:program "git-finder"
-    #:args search-paths
-    (invariant-assertion (listof path-string?) search-paths)
-    (define repos (find-git-repos (gethostname) search-paths))
-    (for-each
-      (λ (repo)
-         (match-define (Repo hostname root locals remotes) repo)
-         (printf "~a ~a~n" hostname root)
-         (printf "\tlocals~n")
-         (for-each (λ (l) (printf "\t\t~a~n" l)) locals)
-         (printf "\tremotes~n")
-         (for-each (λ (r) (printf "\t\t~a~n" r)) remotes)
-         )
-      repos)
-    (eprintf "Found ~a roots ~a locals, ~a remotes.~n"
-             (length repos)
-             (length (append* (map Repo-locals repos)))
-             (length (append* (map Repo-remotes repos))))))
+
+  (let ([out-format 'table])
+    (command-line
+      #:program "git-finder"
+
+      #:once-any
+      [("-t" "--table")
+       "All found repos in a tabular text format."
+       (set! out-format 'table)]
+      [("-g" "--graph-dupes")
+       "Multi-homed repos in DOT language for Graphviz."
+       (set! out-format 'graph)]
+
+      #:args search-paths
+      (invariant-assertion (listof path-string?) search-paths)
+
+      (define output
+        (case out-format
+          [(table) print-table]
+          [(graph) print-graph]))
+      (define repos (find-git-repos (gethostname) search-paths))
+      (output repos)
+      (eprintf "Found ~a roots ~a locals, ~a remotes.~n"
+               (length repos)
+               (length (uniq (append* (map Repo-locals repos))))
+               (length (uniq (append* (map Repo-remotes repos))))))))
