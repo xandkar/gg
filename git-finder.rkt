8bc0a0024106c16ff42f23becf98963d4b850525
Siraaj Khandkar <siraaj@khandkar.net>
2021-12-21 14:58:36 -0500

    Make Remote a child of Local
    and improve presentation.

--- git-finder.rkt
+++ git-finder.rkt
@@ -2,28 +2,27 @@
 
 #lang racket
 
-(require racket/os)
+(require racket/os
+         xml)
+
+(define-struct/contract Remote
+  ([name string?]
+   [addr string?])
+  #:transparent)
 
 (define-struct/contract Local
   ([hostname string?]
    [path path-string?]
-   [description (or/c #f string?)])
+   [description (or/c #f string?)]
+   [remotes (listof Remote?)])
   #:transparent)
 
+;; TODO locals should be a custom set keyed on hostname+path
 (define locals? (listof Local?))
-
-;; TODO Consider making Remote a child of Local
-(define-struct/contract Remote
-  ([hostname string?]
-   [address string?])
-  #:transparent)
-
-(define remotes? (listof Remote?))
 
 (define-struct/contract Repo
   ([root string?]
-   [locals locals?] ; TODO locals should be a custom set keyed on hostname+path
-   [remotes remotes?])
+   [locals locals?])
   #:transparent)
 
 (define/contract (exe cmd)
@@ -44,10 +43,14 @@
   lines)
 
 (define/contract (git-dir->remotes git-dir-path)
-  (-> path-string? (listof string?))
-  (exe
+  (-> path-string? (listof Remote?))
+  (define cmd
     (string-append
-      "git --git-dir=" git-dir-path " remote -v | awk '{print $2}' | sort -u")))
+      "git --git-dir=" git-dir-path " remote -v | awk '{print $1, $2}' | sort -u"))
+  (map (λ (line)
+          (match-define (list name addr) (string-split line))
+          (Remote name addr))
+       (exe cmd)))
 
 (define/contract (git-dir->root-digest git-dir-path)
   (-> path-string? (or/c #f string?))
@@ -68,28 +71,33 @@
 (define uniq
   (compose set->list list->set))
 
+(define/contract (local-dir->description path)
+  (-> path-string? (or/c #f string?))
+  (define path-description (build-path path "description"))
+  (if (file-exists? path-description)
+      (match (file->lines path-description)
+        ['() #f]
+        [(list* line _)
+         (if (string-prefix? line "Unnamed repository;")
+             #f
+             line)])
+      #f))
+
 (define/contract (find-git-repos hostname search-paths exclude-prefix exclude-regexp)
   (-> string? (listof path-string?) (listof path-string?) (listof pregexp?) (listof Repo?))
-  (define (root repos) (first (first repos))) ; All roots are the same in a group
-  (define (locals repos) (map second repos))
-  (define (remotes repos) (uniq (append* (map third repos))))
-  (map (λ (repos-with-shared-root-commit)
-          (Repo (root repos-with-shared-root-commit)
-                (map (λ (path-git)
-                        (define path-description (build-path path-git "description"))
-                        (define description
-                          (if (file-exists? path-description)
-                              (match (file->lines path-description)
-                                ['() #f]
-                                [(list* line _)
-                                 (if (string-prefix? line "Unnamed repository;")
-                                     #f
-                                     line)])
-                              #f))
-                        (Local hostname path-git description))
-                     (locals repos-with-shared-root-commit))
-                (map (λ (addr) (Remote hostname addr))
-                     (remotes repos-with-shared-root-commit))))
+  ; All roots are the same in a group, so root of first repo is as good as any:
+  (define (group->root group) (first (first group)))
+  (define (group->local-dirs group) (map second group))
+  (define (group->remotes group) (uniq (append* (map third group))))
+  (map (λ (group)
+          (define root (group->root group))
+          (define locals
+            (map (λ (local-dir)
+                    (define description (local-dir->description local-dir))
+                    (define remotes (group->remotes group))
+                    (Local hostname local-dir description remotes))
+                 (group->local-dirs group)))
+          (Repo root locals))
        (group-by first
                  (foldl (λ (dir repos)
                            ; TODO git lookups can be done concurrently
@@ -110,18 +118,17 @@
 
 (define/contract (print-table repos)
   (-> (listof Repo?) void?)
-  (define (output root tag get-host get-addr locations)
-    (for-each
-      (λ (l) (displayln (string-join (list root (get-host l) tag (get-addr l)) " ")))
-      locations))
-  (for-each
-    (λ (repo)
-       (match-define (Repo root locals remotes) repo)
-       (output root "local"  Local-hostname  Local-path     locals)
-       (output root "remote" Remote-hostname Remote-address remotes)
-       (newline) ; So that same-root locations are visually grouped.
-       )
-    repos))
+  (define (row . columns) (displayln (string-join columns " ")))
+  (for* ([r repos]
+         [l (Repo-locals r)]
+         [m (if (null? (Local-remotes l))
+                `(,(Remote "" ""))
+                (Local-remotes l))])
+        (row (Repo-root r)
+             (Local-hostname l)
+             (Local-path l)
+             (Remote-name m)
+             (Remote-addr m))))
 
 (define/contract (print-graph repos)
   (-> (listof Repo?) void?)
@@ -130,50 +137,55 @@
   (define all-remotes (mutable-set))
   (define (local-id l) (format "~a:~a" (Local-hostname l) (Local-path l)))
   (define (local-label l)
-    (define description (Local-description l))
+    (define hostname (Local-hostname l))
+    (define description (let ([d (Local-description l)]) (if d d "")))
     (define path (Local-path l))
-    (if description
-        (format "~a~n~a" path description)
-        (format "~a"     path)))
+    (xexpr->string
+      `(table
+        ([border      "0"]
+         [cellborder  "0"]
+         [cellpadding "1"]
+         [cellspacing "0"])
+        (tr (td (b "host ")) (td ([align "left"]) ,hostname))
+        (tr (td (b "path ")) (td ([align "left"]) ,path))
+        (tr (td (b "desc ")) (td ([align "left"]) ,description)))))
   (displayln "digraph {")
   (for-each
     (λ (r)
        ; TODO Color and shape codes for: root, local and remote.
        (match r
-         [(Repo root (and locals (list* _ _ _)) remotes)
+         [(Repo root (and locals (list* _ _ _)))
           (for-each
             (λ (l)
                (set-add! all-roots root)
                (set-add! all-locals l)
                (printf
-                 "~v -> ~v [label=~v, fontname=monospace, fontsize=8];~n"
+                 "~v -> ~v [fontname=monospace, fontsize=8, color=yellowgreen];~n"
                  root
-                 (local-id l)
-                 (Local-hostname l)))
-            locals)
-          (for-each
-            (λ (r)
-               (set-add! all-roots root)
-               (set-add! all-remotes r)
-               (printf
-                 "~v -> ~v [label=~v, fontname=monospace, fontsize=8];~n"
-                 root
-                 (Remote-address r)
-                 (Remote-hostname r)))
-            remotes)]
+                 (local-id l))
+               (for-each
+                 (λ (r)
+                    (set-add! all-remotes (Remote-addr r))
+                    (printf
+                      "~v -> ~v [label=~v, fontname=monospace, fontsize=8, color=lightblue fontcolor=lightblue3, dir=both, arrowtail=dot];~n"
+                      (local-id l)
+                      (Remote-addr r)
+                      (Remote-name r)))
+                 (Local-remotes l)))
+            locals)]
          [_ (void)]))
     repos)
   (set-for-each
     all-roots
     (λ (r)
        (printf
-         "~v [shape=point style=filled, fillcolor=yellowgreen fontcolor=white, fontname=monospace, fontsize=8];~n"
+         "~v [shape=rectangle, style=filled, fillcolor=yellowgreen, fontname=monospace, fontsize=8];~n"
          r)))
   (set-for-each
     all-locals
     (λ (l)
        (printf
-         "~v [label=~v shape=folder, style=filled, fillcolor=wheat, fontname=monospace, fontsize=8];~n"
+         "~v [label=<~a> shape=folder, style=filled, fillcolor=wheat, fontname=monospace, fontsize=8, fontcolor=black];~n"
          (local-id l)
          (local-label l))))
   (set-for-each
@@ -181,7 +193,7 @@
     (λ (r)
        (printf
          "~v [shape=oval, style=filled, fillcolor=lightblue, fontname=monospace, fontsize=8];~n"
-         (Remote-address r))))
+         r)))
   (displayln "}"))
 
 (module+ main
@@ -232,5 +244,5 @@
       (eprintf "Found ~a roots, ~a locals and ~a remotes in ~a seconds.~n"
                (length repos)
                (length (uniq (append* (map Repo-locals repos))))
-               (length (uniq (append* (map Repo-remotes repos))))
+               (length (uniq (flatten (map (λ (locals) (map Local-remotes locals)) (map Repo-locals repos)))))
                (real->decimal-string (/ (- t1 t0) 1000) 3)))))
